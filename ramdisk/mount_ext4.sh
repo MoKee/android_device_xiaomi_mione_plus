24bed15fb413d86805622d2ff7c0f9b5d9ad3a1d
PeterCxy <xqsx43cxy@126.com>
2013-06-26 10:42:16 +0800

    Use mount_ext4.sh from mirom/adnroid_bootable_recovery

--- ramdisk/mount_ext4.sh
+++ ramdisk/mount_ext4.sh
@@ -2,39 +2,23 @@
 export PATH=/system/bin:/system/xbin:$PATH
 BLOCK_DEVICE=$1
 MOUNT_POINT=$2
+LOG_FILE="/dev/null"
+LOG_LOCATION="/data/.fsck_log/"
 
-if [ -e ${BLOCK_DEVICE} ]; then
-    /system/bin/dumpe2fs -h ${BLOCK_DEVICE}
-    ret1=$?
-    if [ $ret1 -ne 0 ];then
-        mke2fs -T ext4 -j -L ${MOUNT_POINT} ${BLOCK_DEVICE}
-        ret2=$?
-        echo "${PART_ALIAS} partition format ret = $ret2"
-        if [ $ret2 -ne 0 ];then
-            exit 1
-        fi
-    fi
-
-    e2fsck -y ${BLOCK_DEVICE}
-    ret3=$?
-    echo "e2fsck on ${BLOCK_DEVICE} ret = $ret3"
-
-    mount -t ext4 -o nosuid,nodev,barrier=1,journal_checksum,noauto_da_alloc ${BLOCK_DEVICE} ${MOUNT_POINT}
-
-    if [ "${MOUNT_POINT}" == "/data" -a -e /data/extend_size ]; then
-        umount ${MOUNT_POINT}
-        e2fsck -f -y ${BLOCK_DEVICE}
-        ret4=$?
-        echo "Forced e2fsck on ${BLOCK_DEVICE} ret = $ret4"
-        resize2fs ${BLOCK_DEVICE}
-        ret5=$?
-        echo "resize ${BLOCK_DEVICE} ret = $ret5"
-        e2fsck -y ${BLOCK_DEVICE}
-        ret6=$?
-        echo "e2fsck on ${BLOCK_DEVICE} ret = $ret6"
-        mount -t ext4 -o nosuid,nodev,barrier=1,journal_checksum,noauto_da_alloc ${BLOCK_DEVICE} ${MOUNT_POINT}
-        if [ $ret5 -eq 0 -o $ret5 -eq 2 ]; then
-            rm ${MOUNT_POINT}/extend_size
-        fi
-    fi
+# storage log
+if [ "${MOUNT_POINT}" == "/storage_int" ]; then
+    mkdir ${LOG_LOCATION}
+    busybox find /data/.fsck_log/ -type f -mtime +7  -exec rm {} \;
+    TIMESTAMP=`date +%F_%H-%M-%S`
+    LOG_FILE=${LOG_LOCATION}/storage_${TIMESTAMP}.log
 fi
+
+# mount partition
+if [ -e ${BLOCK_DEVICE} ]; then
+    mount -t ext4 -o nosuid,nodev,barrier=1,noauto_da_alloc ${BLOCK_DEVICE} ${MOUNT_POINT}
+fi
+
+# hide recovery partition
+RECOVERY_NODE="$(busybox readlink -f /dev/block/platform/msm_sdcc.1/by-name/recovery)"
+busybox mv "${RECOVERY_NODE}" /dev/recovery_moved
+busybox mknod -m 0600 "${RECOVERY_NODE}" b 1 3
